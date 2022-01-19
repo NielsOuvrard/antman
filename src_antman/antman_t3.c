a0c640a75cef39ff3029881192a44962c2a9d7c8
Paul <paul.garcia@epitech.eu>
2022-01-19 04:20:37 -0500

    t3 bi

--- src_antman/antman_t3.c
+++ src_antman/antman_t3.c
@@ -8,88 +8,107 @@
 #include "my.h"
 #include "am_header.h"
 
-int get_nbr(char *yes, int i)
+int get_nbr2(char *yes, int i)
 {
     int res = 0, x = i, neg = 1;
+    if (yes[x] == '-' || yes[x] == '+') {
+        if (yes[x] == '-') 
+            neg = -1;
+        x++;
+    }
     while (yes[x] <= 57 && yes[x] >= 48) {
     res = res * 10;
     res = res + (yes[x] - 48);
     x++;
     }
+    res = res * neg;
     return res;
 }
 
-int len_line(char *file, int i) {
+void readbitsc(char a) {
+    int mask = 1;
 
-    int w = 0, res = 0, e = 0;
-
-    for(e = 0; file[e] != '\0' && w < i; e++) {
-        if (file[e] == '\n')
-            w++;
+    for(int x = 0; x < 8;x++) {
+        my_putunsint(!!(a & mask));
+        mask = mask * 2;
     }
-    while (file[e] != '\n') {
-        e++;
+}
+
+void readbitsint(int a) {
+    int mask = 1;
+    for(int x = 0; x < 8;x++) {
+        my_putunsint(!!(a & mask));
+        mask = mask * 2;
+    }
+}
+
+void printbits(char *it, char *a) {
+    int mask = 1;
+    for(int i = 0; i < 8; i++) {
+        if(it[i] == '1')
+            *a = (*a | mask);
+        else if (it[i] == '0' && (*a & mask) != 0)
+            *a = (*a ^ mask);
+        mask = mask * 2;
+    }
+}
+
+char nbrbi(int a) {
+    int mask = 1;
+    char e = 0x00000000;
+    for(int x = 0; x < 8;x++) {
+        if(!!(a & mask) == 1)
+            e = (e | mask);
+        mask = mask * 2;
+    }
+    return e;
+}
+
+int bi_dec(char i) {
+    int mask = 1, tot = 0;
+
+    for(int x = 0; x < 8; x++) {
+        if(!!(i & mask) == 1)
+            tot = tot + mask;
+
+        mask = mask * 2;
+    }
+    return tot;
+}
+
+int len_line(char *file, int i) {
+    int res = 0;
+    while (file[i] != '\0' && file[i] != '\n') {
+        i++;
         res++;
     }
-    res++;
     return res;
 }
 
 
 int find_start(char *file) {
-    int e = 1, res = len_line(file, 0);
-    while(len_line(file, e) > 4) {
-        res = res + len_line(file, e);
-        e++;
-    }
-    return res;
-}
-
-int nbn(char *file, int start) {
-    int res = 0;
-    while(file[start] != '\0'){
-        if(file[start] == '\n')
-            res++;
-        start++;
-    }
-
-    return res - 1;
-}
-
-void size(char *file, List **list, List **enda) {
-
-    int a = 0, i = find_start(file), res = 0, temp1, temp2, tot = 1, nb = nbn(file, find_start(file));
-
-    for(int w = 0; w < i; w++) {
-        if(file[w] == '\n' /*&& w != i - 1*/)
-            my_putchar('@');
-        else if (file[w] != '\n')
-            my_putchar(file[w]);
-    }
-
-    for(int w = 0; w < nb; w++) {
-        temp1 = get_nbr(file, i);
-        while (file[i] != '\n' && file[i] != '\0')
-            i++;
+    int res = len_line(file, 0), i = 0;
+    while(file[i] != '\n') 
         i++;
-        temp2 = get_nbr(file, i);
-        if(temp1 == temp2)
-            tot++;
-        if(temp1 != temp2) {
-            addend(temp1, tot, enda, list);
-            tot = 1;
-            a++;
-        }
+    i++;
+    while(len_line(file, i) > 4) { 
+        i = i + len_line(file, i);
+        i++;
     }
-    temp1 = get_nbr(file, i);
-    *list = addat(*list, temp1, tot, a);
+    return i;
 }
 
 int type_3 (char *file)
 {
-    List *list = emptylist();
-    List *enda = NULL;
-    size(file, &list, &enda);
-    printlist(list);
+    int i = find_start(file), eee;
+        for (int x = 0; x < i; x++)
+            my_putchar(file[x]);
+        my_putchar('|');  
+        for(i; file[i] != '\0'; i++) {
+            eee = get_nbr2(file, i);
+            my_putchar(nbrbi(eee));
+            for(i; file[i] != '\n' && file[i] != '\0'; i++);   
+            eee = 0;
+        }
     return 0;
 }
--- src_giantman/giantman_t3.c
+++ src_giantman/giantman_t3.c
@@ -8,84 +8,32 @@
 #include "my.h"
 #include "am_header.h"
 
-int get_nbr(char *yes, int i)
-{
-    int res = 0, x = i, neg = 1;
-    while (yes[x] <= 57 && yes[x] >= 48) {
-    res = res * 10;
-    res = res + (yes[x] - 48);
-    x++;
+int bi_dec(char i) {
+    int mask = 1, tot = 0;
+
+    for(int x = 0; x < 8; x++) {
+        if(!!(i & mask) == 1)
+            tot = tot + mask;
+
+        mask = mask * 2;
     }
-    return res;
+    return tot;
 }
-
-int len_line(char *file, int i) {
-
-    int w = 0, res = 0, e = 0;
-
-    for(e = 0; file[e] != '\0' && w < i; e++) {
-        if (file[e] == '@')
-            w++;
-    }
-    while (file[e] != '@') {
-        e++;
-        res++;
-    }
-    res++;
-    return res;
-}
-
-int find_start(char *file) {
-    int e = 1, res = len_line(file, 0);
-    while(len_line(file, e) > 4) {
-        res = res + len_line(file, e);
-        e++;
-    }
-    return res;
-}
-
-void gigi(char *file) {
-    
-    int i = find_start(file), e = 0, temp = 0, nb = 0;
-
-    for(int x = 0; x < i; x++) {
-        if(file[x] == '@')
-            my_putchar('\n');
-        else    
-            my_putchar(file[x]);
-    }
-    for (i ; file[i] != '\0'; i) {
-        if (file[i+1] != '@') {
-            i++;
-            while(file[i] != '@' && file[i] != '\0') {
-                my_putchar(file[i]);
-                i++;
-            }
-            my_putchar('\n');
-        }
-        if (file[i+1] == '@') {
-            i = i + 2;
-            temp = get_nbr(file, i);
-            while(file[i] != '@')
-                i++;
-            i++;
-            nb = get_nbr(file, i);
-            for(int count = 0; count < nb ;count++) {
-                my_putunsint(temp);
-                my_putchar('\n');
-            while(file[i] != '@'  && file[i] != '\0')
-                i++;
-            }
-        }
-    }
-
-
-}
-
 
 int type_3 (char *file)
 {
-    gigi(file);
+    struct stat e;
+    int i = 0;
+
+    stat(file, &e);
+    for(i; file[i] != '|'; i++)
+        my_putchar(file[i]);
+    i++;
+    int size = e.st_size;
+    for(i; i < size; i++) {
+        my_putunsint(bi_dec(file[i]));
+        my_putchar('\n');
+    }
 
     return 0;
 }
